34d1b9d7c1eceee922d1947f7ac95edf669d4cd9
Edward Hunt <57264981+ed7hunt@users.noreply.github.com>
2020-02-06 10:05:56 -0500

    Update server_information.ps1

--- server_information.ps1
+++ server_information.ps1
@@ -1,3 +1,11 @@
+# 1. You need to allow WMI/SMB firewall ports (TCP 135,445) on each target server if they are not already open. Running this command directly on each server should accomplish this.
+#	New-NetFirewallRule -DisplayName "WMI/SMB Firewall Rules: TCP" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 135,445
+#
+# 2. If you changed the firewall, you need to enable it by restarting the firewall services on the server. You could run this command if you are running Windows 10.
+#	If ((Get-Service mpssvc).Status -eq 'Running'){ Restart-Service -Name mpssvc -Force -Verbose }
+#
+# 3. On the switch, you need to allow TCP 135,445 to enable WMI/SMB ports on the firewall on the IP Address range of servers from your source location.
+
 #Modify the path below to this program's location on your computer
 $root_path = "C:\Users\$env:USERNAME\Desktop\"
 #other variables
