759ecb02d0bf342f98dbd1be5813179224d1eddb
Edward Hunt <57264981+ed7hunt@users.noreply.github.com>
2020-02-14 08:00:50 -0500

    Update update_modules.ps1

--- update_modules.ps1
+++ update_modules.ps1
@@ -1,57 +1,47 @@
-﻿Clear
+# If this file were public, you may run this script remotely from GitHub using this command:
+# Invoke-Expression (Invoke-WebRequest https://raw.githubusercontent.com/ed7hunt/PowerShell-and-PowerGUI/master/update_modules.ps1).Content
+Clear
 Write-Host "
        Title: Update Powershell Modules"
-$version="1.0"
+$version="1.1"
 Write-Host "     Version: $version
       Author: Edward Hunt
         Dept: Infrastructure Technology Division
-        Date: Jan 31, 2020
+        Date: Feb 14, 2020
+     Purpose: The purpose of this script is to:
+              a) Enable Execution Policies so your PowerShell scripts can run.
+              b) Update your PowerShell versions to the latest levels.
+              c) Install common Windows PowerShell modules that System Administrators commonly use.
 Requirements: Your source OS should be >= Windows 10, Server 2012 + up
-              If you would like a video tutorial, paste the link below into your browser:
-              
-              https://drive.google.com/file/d/1rA90vXshDwLidOnCIbFg8MhbRVO_qj3y/view?usp=sharing
-              `n"
+Write-Host "              This script should be run as Administrator. Use [SHIFT] + [RIGHT MOUSE CLICK], 'Run as Administrator'" -foregroundcolor Red -Nonewline
 
-
-If ( -NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator" )) {
-    Write-Host "You are not running PowerShell as ADMINISTRATOR." -ForegroundColor Yellow
-    Write-Host "Use [SHIFT] + [RIGHT MOUSE CLICK], 'Run as Administrator'"
-} 
-Else {
-    Write-Host "This script will run as ADMIN with elevated priviledges.`n" -ForegroundColor Green
-    $answer1=$(Read-Host "Would you like to install latest modules and update your PowerShell to the latest version?  [Y] or [N]")
-    If ($answer1 -eq "Y") {
-        Write-Host "Below is the version of PowerShell installed on your computer:"
-        $PSVersionTable
-        Update-Module
-        Write-Host "Follow the instructions on how to update PowerShell. Launching IE...`n" -ForegroundColor Green
-        $InternetExplorer=new-object -com internetexplorer.application
-        $InternetExplorer.navigate2("https://docs.microsoft.com/en-us/powershell/scripting/install/installing-windows-powershell?view=powershell-6")
-        $InternetExplorer.visible=$true
+$Elevated_Privileges=$([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator" )
+Write-Host "Running as USERID = " -NoNewline
+Write-Host "$(whoami)" -ForegroundColor Yellow
+Write-Host "Elevated Privileges = $Elevated_Privileges`n"
+If ($Elevated_Privileges) {    
+    Write-Host "Adjusting your Execution policies so this script can properly run. " -ForegroundColor Green
+    Write-Host "Listing Execution policy BEFORE making changes..."
+    $EPout=$(Get-ExecutionPolicy -List | Out-String)
+    Write-Host $EPout -ForegroundColor Cyan
+    Read-Host "Hit [ENTER] to continue"
+    $ErrorActionPreference="silentlycontinue"
+    Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+    $ErrorActionPreference="stop"
+    Write-Host "Listing Execution policy AFTER making changes..."
+    $EPout=$(Get-ExecutionPolicy -List | Out-String)
+    Write-Host $EPout -ForegroundColor Cyan
+    Read-Host "Hit [ENTER] to continue"
+    $update_modules=$(Read-Host "To update PowerShell modules type 'Y', else hit [ENTER]")
+    If ($update_modules -eq "Y") {
+        "Microsoft.Powershell.Management","Get-WmiObject","TCPClient","SqlServer","ServerManager","ActiveDirectory","Microsoft.Powershell.Management"" | Foreach { Get-Module $_ }
     }
-    $answer2=$(Read-Host "Would you like to update your policies so you can run Powershell?  [Y] or [N]")
-    If ($answer2 -eq "Y"){
-        If ($(Get-ExecutionPolicy -scope LocalMachine) -ne "Unrestricted") {
-            Write-Host "Attempting to set Execution Policy for scope LocalMachine was set to Unrestricted." -ForegroundColor Green
-            Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
-            $Change=$True
-        }
-        If ($(Get-ExecutionPolicy -scope CurrentUser) -ne "RemoteSigned") {
-            Write-Host "Attempting to set Execution Policy for scope CurrentUser to RemoteSigned." -ForegroundColor Green
-            Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-            $Change=$True
-        }
-        If ($Change) {
-            Write-Host "`nDone making changes to Execution Policies. See output below:" -ForegroundColor Green
-        }
-        Else { 
-            Write-Host "`nYour Execution Policies look OK here. No changes were made." -ForegroundColor Green
-        }
-        Get-Executionpolicy -list | Out-String
-    }
-    $answer3=$(Read-Host "Would you like to update other modules such as SQLServer, ServerManager, ActiveDirectory, Microsoft.Powershell.Management? [Y] or [N]")
-    If ($answer3 -eq "Y") {
-        "SqlServer","ServerManager","ActiveDirectory","Microsoft.Powershell.Management" | Foreach { Import-Module $_ }
-    }
+    Start-Sleep -s 5
     Write-Host "`nComplete"
 }
+Else {
+    Write-Host "You are not running PowerShell as ADMINISTRATOR." -ForegroundColor Yellow
+    Write-Host "Use [SHIFT] + [RIGHT MOUSE CLICK], 'Run as Administrator'"
+}    
