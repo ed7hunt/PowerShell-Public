fdfcdf9eacb908e57e441c730b923ba94fd90a0c
Edward Hunt <57264981+ed7hunt@users.noreply.github.com>
2020-02-14 10:05:54 -0500

    Update update_powershell.ps1

--- update_powershell.ps1
+++ update_powershell.ps1
@@ -3,11 +3,11 @@
 #
 # If you work in place such as ours in which the "POWERS THAT BE" do not want you running scripts over the network, you may find that
 # PowerShell might not be enabled. Your execution policies could be disabled by group-policy: either on your computer, user account or 
-# both. This script is valuable to some because you can undo the security policy that was created to "keep you safe". It would be wise 
-# to seek approval by your security offials before running this script.
+# both. This script is therefore valuable because you can undo the security policy that was created to "keep you safe". It would be 
+# wise to seek approval by your security offials or manager before running this script.
 # 
-# Be cautious about updating .NET if you are confronted with this scenario. You may be successful in updating it; but end up breaking
-# applications that may be dependent upon it in a server related environment. With that being said, good luck!
+# Be extremely cautious about updating .NET if you are confronted with this scenario. You may be successful in updating it; but end up
+# breaking applications that may be dependent upon it in a server related environment. With that being said, good luck!
 
 Clear
 Write-Host "
